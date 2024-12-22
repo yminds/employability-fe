f3a48598e699978e5bb0f36707b033d236bb00ba
developers-yminds <developers@yminds.ai>
2024-12-23 01:22:45 +0530

    css changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
-  	extend: { 
+  	extend: {
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
