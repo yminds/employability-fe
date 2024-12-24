e900c81f5581ef1dc9a6448a77579f41295351db
developers <developers@yminds.ai>
2024-12-24 17:16:55 +0530

    Updated Goals

--- src/features/dashboard/GoalList.tsx
+++ src/features/dashboard/GoalList.tsx
@@ -59,7 +59,7 @@ const GoalList: React.FC<Props> = () => {
                         />
                         <div className="flex flex-col p-6 justify-center items-start gap-2 self-stretch">
                             <h3 className="text-gray-800 text-base font-medium leading-5">{goal.title}</h3>
-                            <p className="text-gray-600 text-base font-normal leading-6 tracking-wide">{goal.description}</p>
+                            <p className="text-gray-600 text-base font-normal leading-6 tracking-wide line-clamp-2">{goal.description}</p>
                         </div>
                     </div>
                 ))}
