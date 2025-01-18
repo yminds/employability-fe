afc840e6a0e475c688b3e75e70a07e98fac595c7
Nawaz Pasha <nawaz.pasha@yminds.ai>
2025-01-18 12:24:30 +0530

    Update Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import EnvironmentPlugin from "vite-plugin-environment";
- 
+
 export default defineConfig({
   resolve: {
     alias: {
       "@": "/src",
     },
   },
-  plugins: [react(), tsconfigPaths(), EnvironmentPlugin("all")],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    EnvironmentPlugin([
+      "VITE_X_API_KEY",
+      "VITE_API_BASE_URL",
+      "AWS_ACCESS_KEY_ID",
+      "AWS_SECRET_ACCESS_KEY",
+      "AWS_REGION",
+      "AWS_S3_BUCKET_NAME",
+    ]),
+  ],
 });
