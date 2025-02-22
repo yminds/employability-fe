3b5ee243fe70a1d61bdccd3c3b6d3a43b626aec6
dilshad-yminds <dilshad@yminds.ai>
2025-02-22 13:19:36 +0530

    refactor: clean up code and improve logging in Interview components

--- src/components/interview/Interview.tsx
+++ src/components/interview/Interview.tsx
@@ -110,7 +110,7 @@ const Interview: React.FC<{
       }
     };
 
-    
+
 
     const handleAIResponse = (data: string) => {
       setInterviewState("SPEAKING"); // Move to SPEAKING when AI starts speaking
@@ -255,16 +255,17 @@ const Interview: React.FC<{
       provider: "openai",
       _id: interviewDetails.data._id,
       thread_id: interviewDetails.data.thread_id,
-      user_id: interviewDetails.data.user_id,
+      user_id: interviewDetails.data.user_id, 
       user_skill_id: interviewDetails.data.user_skill_id,
       skill_id: interviewDetails.data.skill_id,
       code_snippet: question.codeSnippet?.code || "",
       question: question.question,
       skill_name: interviewTopic,
-      concepts: concepts.slice(0,3 ),
+      concepts: concepts,
       interview_id: interviewDetails.data._id,
       level: user?.experience_level || "entry"
     }).unwrap();
+    
 
     console.log("response", response);
 
@@ -340,6 +341,9 @@ const LayoutBuilder = ({
 }: LayoutBuilderProps) => {
   const [isSidebarOpen, setSidebarOpen] = useState<boolean>(false);
 
+
+  console.log("all conepts", concepts);
+  
   // console.log("concepts", concepts);
   const coveredConceptsLength = concepts.filter((concept) => concept.status === "completed").length;
   const calculateProgress = (concepts: any[]) => {
